c9554822894c3a3516e0c5a6d65427b9852a852f
Will Merrill <lambdaviking@google.com>
2023-02-22 04:08:50 +0000

    Final check in

--- scripts/experiments.sh
+++ scripts/experiments.sh
@@ -2,17 +2,17 @@
 
 # === Suffix DFA experiments ===
 
-OUTPUT_DIR=out/suffix_dfa-1
-mkdir ${OUTPUT_DIR}
-BUILD_METHOD=suffix_dfa MAX_POINTERS=1 OUTPUT_DIR=${OUTPUT_DIR} source scripts/run.sh
+# OUTPUT_DIR=out/suffix_dfa-1
+# mkdir ${OUTPUT_DIR}
+# BUILD_METHOD=suffix_dfa MAX_POINTERS=1 OUTPUT_DIR=${OUTPUT_DIR} source scripts/run.sh
 
-OUTPUT_DIR=out/suffix_dfa-1-f5
-mkdir ${OUTPUT_DIR}
-BUILD_METHOD=suffix_dfa MAX_POINTERS=1 MIN_LENGTH=5 OUTPUT_DIR=${OUTPUT_DIR} source scripts/run.sh
+# OUTPUT_DIR=out/suffix_dfa-1-f5
+# mkdir ${OUTPUT_DIR}
+# BUILD_METHOD=suffix_dfa MAX_POINTERS=1 MIN_LENGTH=5 OUTPUT_DIR=${OUTPUT_DIR} source scripts/run.sh
 
 OUTPUT_DIR=out/suffix_dfa-1-t3
 mkdir ${OUTPUT_DIR}
-BUILD_METHOD=suffix_dfa MAX_POINTERS=1 TEMP=0.33 OUTPUT_DIR=${OUTPUT_DIR} source scripts/run.sh
+BUILD_METHOD=suffix_dfa MAX_POINTERS=1 TEMP=0.3 OUTPUT_DIR=${OUTPUT_DIR} source scripts/run.sh
 
 # === Oracle experiments ===
 
@@ -20,9 +20,9 @@ OUTPUT_DIR=out/oracle-1-f5
 mkdir ${OUTPUT_DIR}
 BUILD_METHOD=oracle MAX_POINTERS=1 MIN_LENGTH=5 OUTPUT_DIR=${OUTPUT_DIR} source scripts/run.sh
 
-OUTPUT_DIR=out/oracle-1-t5
+OUTPUT_DIR=out/oracle-1-t3
 mkdir ${OUTPUT_DIR}
-BUILD_METHOD=oracle MAX_POINTERS=1 TEMP=0.2 OUTPUT_DIR=${OUTPUT_DIR} source scripts/run.sh
+BUILD_METHOD=oracle MAX_POINTERS=1 TEMP=0.3 OUTPUT_DIR=${OUTPUT_DIR} source scripts/run.sh
 
 # OUTPUT_DIR=out/linear
 # mkdir ${OUTPUT_DIR}
